90ba3446545ac6e8cd5c62222f100b31eb01f006
quang2002 <trieudinhquang2002@gmail.com>
2025-03-21 02:08:34 +0700

    fix: remove echo

--- npm-publish/entrypoint.sh
+++ npm-publish/entrypoint.sh
@@ -4,9 +4,7 @@
 set -e
 
 # Run npm-auth-to-token
-echo "RUN: npm-auth-to-token -u $2 -p $3 -e $4 -r $5" >> $GITHUB_OUTPUT
-sh -c "npm-auth-to-token -u $2 -p $3 -e $4 -r $5" >> $GITHUB_OUTPUT
+sh -c "npm-auth-to-token -u $2 -p $3 -e $4 -r $5"
 
 # Publish package
-echo "RUN: npm publish --registry $5 $1" >> $GITHUB_OUTPUT
-sh -c "npm publish --registry $5 $1" >> $GITHUB_OUTPUT
+sh -c "npm publish --registry $5 $1"
